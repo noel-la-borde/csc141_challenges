3623000436313c4498e8d2019fa47ab1fc64f7ae
Noel La Borde <138098592+noel-la-borde@users.noreply.github.com>
2024-09-09 15:40:30 -0400

    Add files via upload

---
+++ lab3Starter.py
@@ -0,0 +1,17 @@
+# Noel La Borde
+# Date: 08/06/2024
+# This is the starter file for CSC141 lab day 3
+
+
+print("To generate your introduction, I will need some information from you...")
+
+name= input ("What is your name?")
+
+age= input ("How old are you?")
+
+major= input ("What do you study?")
+
+
+print ("Hi, " "My name is " + name + "and I am " + age + " years old!","At Albright College, " "I study " + major + ".") 
+
+
